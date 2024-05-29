0451228d585d215ad734eab20f00adcd9c7dfe89
Felipe Barreiro <anyuba123@gmail.com>
2024-05-29 17:20:08 -0500

    m

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,10 @@
-
-
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const path = require('path')
 require('dotenv').config()
 const connectDB = require('./config/db')
 const router = require('./routes')
-
 
 const app = express()
 app.use(cors({
@@ -16,15 +14,20 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
 app.use("/api", router)
 
-const PORT = 8080 || process.env.PORT
-
+const PORT = process.env.PORT || 8080
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("connnect to DB")
-    console.log("Server is running " + PORT)
+    console.log("connect to DB")
+    console.log("Server is running on port " + PORT)
   })
 })
-
