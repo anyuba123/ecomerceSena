87e25b3aff913b1b19d16f74ed9f525d63b4a668
Felipe Barreiro <anyuba123@gmail.com>
2024-05-30 17:03:11 -0500

    cambios fron y back

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,4 +1,5 @@
- const backendDomin = "http://localhost:8080" 
+/*  const backendDomin = "http://localhost:8080"  */
+const backendDomin = "https://ecomercesena-80.onrender.com";
 
 
 const SummaryApi = {
